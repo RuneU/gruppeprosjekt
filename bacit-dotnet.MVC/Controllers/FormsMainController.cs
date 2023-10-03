e1da2ef848e2ce05c8eca703f7311fdaacf48836
NorwegianKiwi-glitch <simonhelgen1@gmail.com>
2023-10-03 22:57:27 +0200

    enkel side for serviceordre og Formmain controllere

---
+++ bacit-dotnet.MVC/Controllers/FormsMainController.cs
@@ -0,0 +1,16 @@
+﻿using bacit_dotnet;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using bacit_dotnet.MVC.Models;
+using bacit_dotnet.MVC.Views.FormsMain;
+
+namespace bacit_dotnet.MVC.Controllers
+{
+    public class FormsMainController : Controller
+    {
+        public IActionResult ServiceOrdre()
+        {
+            return View();
+        }
+    }
+}
