a05063bd29c5769e2cdf759cc8d545bf812616a0
RuneU <runeundheim@live.no>
2023-10-23 11:56:03 +0200

    Progress on legge til ansatt

---
+++ bacit-dotnet.MVC/Models/CreateUser/CreateUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace bacit_dotnet.MVC.Models.CreateUser;
+
+public class CreateUserViewModel
+{
+    public string Fornavn { get; set; }
+    public string Etternavn { get; set; }
+    public string Email { get; set; }
+    public int AnsattNr { get; set; }
+    public string Rolle { get; set; }
+    public string TelefonNr { get; set; }
+    public string Passord { get; set; }
+    public string BekreftPassord { get; set; }
+    public string Adresse { get; set; }
+    public string Postnummer { get; set; }
+}
