da1170e1b192e742a1ebf25db31373da840f7560
RuneU <runeundheim@live.no>
2023-10-24 13:37:34 +0200

    Added CSRF completely

--- bacit-dotnet.MVC/Program.cs
+++ bacit-dotnet.MVC/Program.cs
@@ -1,7 +1,8 @@
 
     using bacit_dotnet.MVC;
+using Microsoft.AspNetCore;
 
-    var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
     builder.Services.AddControllersWithViews();
@@ -37,9 +38,9 @@
     app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-    
 
-    WebHost.CreateDefaultBuilder(args)
+
+WebHost.CreateDefaultBuilder(args)
     .ConfigureKestrel(c => c.AddServerHeader = false)
     .UseStartup<Startup>()
     .Build();
