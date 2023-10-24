77ba4d53cbdef4a98f78a94732b9713c0b102a10
RuneU <runeundheim@live.no>
2023-10-24 11:49:37 +0200

    fixed small problem in Program.cs still problems to fix

--- bacit-dotnet.MVC/Program.cs
+++ bacit-dotnet.MVC/Program.cs
@@ -1,4 +1,6 @@
 
+    using bacit_dotnet.MVC;
+
     var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -35,6 +37,7 @@
     app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+    
 
     WebHost.CreateDefaultBuilder(args)
     .ConfigureKestrel(c => c.AddServerHeader = false)
