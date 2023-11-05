690348731254aa67636fb263e55ac724d407dfc4
RuneU <runeundheim@live.no>
2023-11-05 14:44:34 +0100

    Changed DB script. Still need changes depending on need

--- CreateDb.sql
+++ CreateDb.sql
@@ -1,104 +1,71 @@
---drop database webAppDatabase;
-create database if not exists webAppDatabase1;
-use webAppDatabase1;
-create table if not EXISTS users
+create table if not exists Customer
 (
-    Id int not null unique auto_increment,
-    Name varchar(255),
-    Email varchar(255) UNIQUE,
-   
-    CONSTRAINT U_User_ID_PK PRIMARY KEY (Id)
+    CustomerID     bigint auto_increment
+        primary key,
+    ServiceOrderID bigint       not null,
+    FirstName      varchar(255) not null,
+    LastName       varchar(255) not null,
+    CustomerEmail  varchar(255) not null,
+    Adress         varchar(255) not null,
+    ZipCode        varchar(255) not null,
+    PhoneNumber    varchar(255) not null
 );
 
-create table if not EXISTS AspNetRoles
+create table if not exists People
 (
-    Id varchar(255) not null,
-    Name varchar(255),
-    NormalizedName  varchar(255),
-    ConcurrencyStamp  varchar(255),
-    CONSTRAINT U_ROLE_ID_PK PRIMARY KEY (Id)
-);
-insert into AspNetRoles(id, Name, NormalizedName) values('Administrator', 'Administrator', 'Administrator');
-create table if not EXISTS AspNetUsers
-(
-         Id varchar(255) not null unique,
-         UserName varchar(255),
-         NormalizedUserName varchar(255),
-         Email varchar(255),
-         NormalizedEmail varchar(255),
-         EmailConfirmed bit not null,
-         PasswordHash varchar(255),
-         SecurityStamp varchar(255),
-         ConcurrencyStamp varchar(255),
-         PhoneNumber varchar(50),
-         PhoneNumberConfirmed bit not null,
-         TwoFactorEnabled bit not null,
-         LockoutEnd TIMESTAMP,
-         LockoutEnabled bit not null,
-         AccessFailedCount int not null,
-          CONSTRAINT PK_AspNetUsers PRIMARY KEY (Id)
-);
-create table if not EXISTS AspNetUserTokens
-(
-    UserId varchar(255) not null,
-    LoginProvider varchar(255) not null ,
-    Name  varchar(255) not null,
-    Value  varchar(255),
-    CONSTRAINT PK_AspNetUserTokens PRIMARY KEY (UserId, LoginProvider)
+    Id        int auto_increment
+        primary key,
+    FirstName varchar(255) null,
+    LastName  varchar(255) null
 );
 
-create table if not EXISTS AspNetRoleClaims
+create table if not exists ServiceOrder
 (
-    Id int UNIQUE auto_increment,
-    ClaimType varchar(255) not null ,
-    ClaimValue  varchar(255) not null,
-    RoleId  varchar(255),
-    CONSTRAINT PK_AspNetRoleClaims PRIMARY KEY (Id),
-    foreign key(RoleId) 
-        references AspNetRoles(Id)
-);      
-
- create table if not EXISTS AspNetUserClaims
-(
-    Id int UNIQUE auto_increment,
-    ClaimType varchar(255) ,
-    ClaimValue  varchar(255),
-    UserId  varchar(255),
-    CONSTRAINT PK_AspNetRoleClaims PRIMARY KEY (Id),
-    foreign key(UserId) 
-        references AspNetUsers(Id)
-);           
-
- create table if not EXISTS AspNetUserLogins
-(
-    LoginProvider int UNIQUE auto_increment,
-    ProviderKey varchar(255) not null ,
-    ProviderDisplayName  varchar(255) not null,
-    UserId  varchar(255) not null,
-    CONSTRAINT PK_AspNetUserLogins PRIMARY KEY (LoginProvider),
-    foreign key(UserId) 
-        references AspNetUsers(Id)
-);         
-
- create table if not EXISTS AspNetUserRoles
-(
-    UserId varchar(255) not null,
-    RoleId varchar(255) not null,
-    CONSTRAINT PK_AspNetUserRoles PRIMARY KEY (UserId,RoleId),
-    foreign key(UserId) 
-        references AspNetUsers(Id),
-    foreign key(RoleId) 
-        references AspNetRoles(Id)
-); 
-
-create table if not EXISTS Kunder
-(
-    Id varchar(255) not null,
-    Name varchar(255),
-    NormalizedName  varchar(255),
-    ConcurrencyStamp  varchar(255),
-    CONSTRAINT U_ROLE_ID_PK PRIMARY KEY (Id)
+    ServiceOrderID      bigint auto_increment
+        primary key,
+    CreatedBy           varchar(255) null,
+    OrderNumber         varchar(255) not null,
+    RecievedDate        datetime     not null,
+    ModelYear           varchar(255) not null,
+    SerialNumber        varchar(255) not null,
+    ServiceRepGuarantee varchar(255) not null,
+    CustomerAgreement   varchar(255) not null
 );
 
-        
-     
+create table if not exists Employees
+(
+    EmployeeID     bigint auto_increment
+        primary key,
+    ServiceOrderID bigint       not null,
+    FirstName      varchar(255) not null,
+    LastName       varchar(255) not null,
+    EmployeeEmail  varchar(255) not null,
+    Adress         varchar(255) not null,
+    ZipCode        varchar(255) not null,
+    PhoneNumber    varchar(255) not null,
+    Role           varchar(255) not null,
+    Password       varchar(255) not null,
+    constraint ServiceOrderID
+        foreign key (ServiceOrderID) references ServiceOrder (ServiceOrderID)
+);
+
+create table if not exists WorkForm
+(
+    WorkFormID                   bigint auto_increment
+        primary key,
+    ServiceOrderID               bigint       not null,
+    BookedService                varchar(255) not null,
+    InquiryRegRec                varchar(255) not null,
+    CaseDone                     varchar(255) not null,
+    RecievedDate                 date         not null,
+    ModellYear                   varchar(255) not null,
+    ProductType                  varchar(255) not null,
+    RecievedDelivery             varchar(255) not null,
+    ServiceDone                  varchar(255) not null,
+    HoursService                 varchar(255) not null,
+    OrderDescriptionFromCustomer varchar(255) not null,
+    CustomerInfo                 varchar(255) not null,
+    OrderNumber                  varchar(255) not null,
+    ServiceForm                  varchar(255) not null
+);
+
