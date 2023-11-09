4e877ba20c1f97c96b08c58886d437858e238e1b
andreas morkesdal <andreas.morkesdal@gmail.com>
2023-11-09 14:06:10 +0100

    script

--- CreateDb.sql
+++ CreateDb.sql
@@ -1,107 +1,99 @@
-create table People
+
+
+docker run --rm --name NoostedDatabase -p 3306:3306/tcp -v "%cd%\database":/var/lib/mysql -e MYSQL_ROOT_PASSWORD=Gruppe4! -d mariadb:10.5.11
+docker exec -it NoestedDatabase mysql -p
+
+
+CREATE DATABASE IF NOT EXISTS NoestedDatabase;
+
+USE NoestedDatabase;
+
+create table Customer
 (
-    Id        int auto_increment
+    CustomerID     int auto_increment
         primary key,
-    FirstName varchar(255) null,
-    LastName  varchar(255) null
+    FirstName      varchar(255) null,
+    LastName       varchar(255) null,
+    CustomerEmail  varchar(255) null,
+    Adress         varchar(255) null,
+    ZipCode        varchar(255) null,
+    PhoneNumber    varchar(255) null
 );
+
 
 create table ServiceOrder
 (
-    ServiceOrderID      bigint auto_increment
+    ServiceOrderID           int auto_increment
         primary key,
-    CreatedBy           varchar(255) null,
-    OrderNumber         varchar(255) not null,
-    RecievedDate        datetime     not null,
-    ModelYear           varchar(255) not null,
-    SerialNumber        varchar(255) not null,
-    ServiceRepGuarantee varchar(255) not null,
-    CustomerAgreement   varchar(255) not null
+    CustomerID               int not null,
+    CreatedBy                varchar(255) null,
+    DateReceived             date         null,
+    ModelYear                varchar(255) null,
+    ProductType              varchar(255) null,
+    SerialNumber             varchar(255) null,
+    ServiceType              varchar(255) null,
+    WhatIsAgreedWithCustomer text         null,
+    RepairDescription        text         null,
+    IncludedParts            varchar(255) null,
+    DateCompleted            date         null,
+    WorkingHours             int          null,
+    ReplacedPartsReturned    varchar(255) null,
+    ShippingMethod           varchar(255) null,
+    Status                   varchar(255) null,
+    Subject                  varchar(255) null,
+    BookedServiceToWeek varchar(255) null,
+    AgreedDeliveryDateWithCustomer date null,
+    constraint ServiceOrder_Customer_CustomerID_fk
+        foreign key (CustomerID) references Customer (CustomerID)
 );
 
 create table CheckList
 (
-    CheckListID         bigint auto_increment
+    CheckListID         int auto_increment
         primary key,
-    ServiceOrderID      bigint       not null,
-    DokNr               varchar(255) not null,
-    Date                datetime     not null,
-    ApprovedBy          varchar(255) not null,
-    CheckClutch         varchar(255) not null,
-    CheckStorage        varchar(255) not null,
-    CheckPto            varchar(255) not null,
-    CheckChainTensioner varchar(255) not null,
-    CheckWire           varchar(255) not null,
-    CheckPinionStorage  varchar(255) not null,
-    CheckSprocket       varchar(255) not null,
-    CheckHydraulic      varchar(255) not null,
-    CheckHose           varchar(255) not null,
-    CheckHydraulicBlock varchar(255) not null,
-    CheckOilTank        varchar(255) not null,
-    CheckOilBox         varchar(255) not null,
-    CheckRingCylinder   varchar(255) not null,
-    CheckBrakeCylinder  varchar(255) not null,
-    CheckWiring         varchar(255) not null,
-    CheckRadio          varchar(255) not null,
-    CheckButtonBox      varchar(255) not null,
-    CheckFunctions      varchar(255) not null,
-    PullingPower        varchar(255) not null,
-    BrakePower          varchar(255) not null,
-    constraint CheckList_ServiceOrder_ServiceOrderID_fk
-        foreign key (ServiceOrderID) references ServiceOrder (ServiceOrderID)
+    CustomerID      int       not null,
+    DokNr               varchar(255) null,
+    Date                datetime     null,
+    ApprovedBy          varchar(255) null,
+    CheckClutch         varchar(255) null,
+    CheckStorage        varchar(255) null,
+    CheckPto            varchar(255) null,
+    CheckChainTensioner varchar(255) null,
+    CheckWire           varchar(255) null,
+    CheckPinionStorage  varchar(255) null,
+    CheckSprocket       varchar(255) null,
+    CheckHydraulic      varchar(255) null,
+    CheckHose           varchar(255) null,
+    CheckHydraulicBlock varchar(255) null,
+    CheckOilTank        varchar(255) null,
+    CheckOilBox         varchar(255) null,
+    CheckRingCylinder   varchar(255) null,
+    CheckBrakeCylinder  varchar(255) null,
+    CheckWiring         varchar(255) null,
+    CheckRadio          varchar(255) null,
+    CheckButtonBox      varchar(255) null,
+    CheckFunctions      varchar(255) null,
+    PullingPower        varchar(255) null,
+    BrakePower          varchar(255) null,
+    constraint CheckList_Customer_CustomerID_fk
+        foreign key (CustomerID) references Customer (CustomerID)
 );
 
-create table Customer
-(
-    CustomerID     bigint auto_increment
-        primary key,
-    ServiceOrderID bigint       not null,
-    FirstName      varchar(255) not null,
-    LastName       varchar(255) not null,
-    CustomerEmail  varchar(255) not null,
-    Adress         varchar(255) not null,
-    ZipCode        varchar(255) not null,
-    PhoneNumber    varchar(255) not null,
-    constraint Customer_ServiceOrder_ServiceOrderID_fk
-        foreign key (ServiceOrderID) references ServiceOrder (ServiceOrderID)
-);
 
-create table Employees
-(
-    EmployeeID     bigint auto_increment
-        primary key,
-    ServiceOrderID bigint       not null,
-    FirstName      varchar(255) not null,
-    LastName       varchar(255) not null,
-    EmployeeEmail  varchar(255) not null,
-    Adress         varchar(255) not null,
-    ZipCode        varchar(255) not null,
-    PhoneNumber    varchar(255) not null,
-    Role           varchar(255) not null,
-    Password       varchar(255) not null,
-    constraint ServiceOrderID
-        foreign key (ServiceOrderID) references ServiceOrder (ServiceOrderID)
-);
 
-create table WorkForm
+create table Employee
 (
-    WorkFormID                   bigint auto_increment
+    EmployeeID     int auto_increment
         primary key,
-    ServiceOrderID               bigint       not null,
-    BookedService                varchar(255) not null,
-    InquiryRegRec                varchar(255) not null,
-    CaseDone                     varchar(255) not null,
-    RecievedDate                 date         not null,
-    ModellYear                   varchar(255) not null,
-    ProductType                  varchar(255) not null,
-    RecievedDelivery             varchar(255) not null,
-    ServiceDone                  varchar(255) not null,
-    HoursService                 varchar(255) not null,
-    OrderDescriptionFromCustomer varchar(255) not null,
-    CustomerInfo                 varchar(255) not null,
-    OrderNumber                  varchar(255) not null,
-    ServiceForm                  varchar(255) not null,
-    constraint WorkForm_ServiceOrder_ServiceOrderID_fk
+    ServiceOrderID int       not null,
+    FirstName      varchar(255) null,
+    LastName       varchar(255) null,
+    EmployeeEmail  varchar(255) null,
+    Adress         varchar(255) null,
+    ZipCode        varchar(255) null,
+    PhoneNumber    varchar(255) null,
+    Role           varchar(255) null,
+    constraint Employee_ServiceOrder_ServiceOrderID_fk
         foreign key (ServiceOrderID) references ServiceOrder (ServiceOrderID)
 );
 
